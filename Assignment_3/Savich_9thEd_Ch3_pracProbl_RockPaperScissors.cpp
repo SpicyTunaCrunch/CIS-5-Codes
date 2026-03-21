3ff75106dbe273b8f3b8e25f1ec22298fa9771a3
Luis <luigy199601@gmail.com>
2026-03-21 02:38:07 -0700

    incomplete

---
+++ Assignment_3/Savich_9thEd_Ch3_pracProbl_RockPaperScissors.cpp
@@ -0,0 +1,26 @@
+/*
+*File: Savitch 9th Edition Chapter 3 Practice Problem 1 
+*Author: Luis Roldan
+*Created on March 21, 2026
+*Purpose: Rock Paper Scissors
+*/
+
+//Sys Libraries
+#include <iostream> //Input-Output Library 
+using namespace std; //Entity Orgainizer
+
+//Main f(x)
+int main(int argc, char** argv){
+    //Delcare Variables
+    char 
+    P, R, S; //P == Papper, R == Rock, S == Scissors
+    short int
+    p1, p2; //Player 1 & Player 2
+
+    //Initialiation 
+    cout << "Rock Papper Scissors Game\nInput Player 1 and Player 2 Choices\n";
+    cin >> p1 >> p2;
+
+    //Mappping
+    
+}
