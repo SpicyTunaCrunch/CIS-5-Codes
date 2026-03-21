651fc90619650a2f5159335cf785ed22080b9d00
Luis <luigy199601@gmail.com>
2026-03-21 13:30:11 -0700

    Case 5 error

--- Assignment_3/Savich_9thEd_Ch3_pracProbl_RockPaperScissors.cpp
+++ Assignment_3/Savich_9thEd_Ch3_pracProbl_RockPaperScissors.cpp
@@ -13,14 +13,23 @@ using namespace std; //Entity Orgainizer
 int main(int argc, char** argv){
     //Delcare Variables
     char 
-    P, R, S; //P == Papper, R == Rock, S == Scissors
-    short int
+    P, R, S, p, r, s, //P == Papper, R == Rock, S == Scissors
     p1, p2; //Player 1 & Player 2
 
     //Initialiation 
-    cout << "Rock Papper Scissors Game\nInput Player 1 and Player 2 Choices\n";
+    cout << "Rock Paper Scissors Game\nInput Player 1 and Player 2 Choices\n";
     cin >> p1 >> p2;
 
     //Mappping
-    
+    if ((p1 == 'P' && p2 == 'R') || (p1 == 'p' && p2 == 'r') || (p1 == 'R' && p2 == 'P') || (p1 == 'r' && p2 == 'p')){
+        cout << "Paper covers rock.";}
+    else if ((p1 == 'R' && p2 == 'S') || (p1 == 'r' && p2 == 's') || (p1 == 'S' && p2 == 'R') || (p1 == 's' && p2 == 'r')){
+        cout << "Rock breaks scissors.";}
+    else if ((p1 == 'P' && p2 == 'S') || (p1 == 'p' && p2 == 's') || (p1 == 'S' && p2 == 'P') || (p1 == 's' && p2 == 'p')) {
+        cout << "Scissors cuts paper.";}
+    else if ((p1 == 'R' && p2 == 'R') || (p1 == 'r' && p2 == 'r') || (p1 == 'R' && p2 == 'r') || (p1 == 'r' && p2 == 'R') ||
+        (p1 == 'S' && p2 == 'S') || (p1 == 's' && p2 == 's') || (p1 == 's' && p2 == 'S') || (p1 == 'S' && p2 == 's') || 
+        (p1 == 'P' && p2 == 'P') || (p1 == 'p' && p2 == 'p') || (p1 == 'P' && p2 == 'p') || (p1 == 'p' && p2 == 'P')) {             
+        cout << "Nobody wins.";}
+        
 }
