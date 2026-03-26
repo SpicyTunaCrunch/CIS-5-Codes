b19856104f4fdae18b1108fd1c5f583d751dddb8
Luis <luigy199601@gmail.com>
2026-03-25 17:40:46 -0700

    incomplete

---
+++ Assignment_4/Gaddis_9thEd_Ch5_Prob1_Sum.cpp
@@ -0,0 +1,27 @@
+/*
+*File: Gaddis 9th Edition Chapter 5 Problem 1
+*Author: Luis Roldan
+*Created on March 25, 2026
+*Purpose: Sum the Sequence
+*/
+
+//sys libraries
+#include <iostream> //Input-output library
+using namespace std; //entity organizer
+
+//main f(x)
+int main(int argc,char** argv){
+
+    //Variable delcaration
+    unsigned int 
+    num,
+    val;
+
+    //Initialization 
+    cin >> num;
+    while () {
+    }
+    cout << "Sum = " << val;
+
+    return 0;
+}
