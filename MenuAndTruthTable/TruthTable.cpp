ca1753814a826f40e777362d598d13fc99c4c393
Luis <luigy199601@gmail.com>
2026-03-26 09:31:11 -0700

    incomplete

---
+++ MenuAndTruthTable/TruthTable.cpp
@@ -0,0 +1,34 @@
+/*
+*File: Turth Table
+*Author: Luis Roldan
+*Created  March 35, 2026
+*/
+
+//Sys Libraires
+#include <iostream> //Inout-Output Library
+using namespace std; //Entity Organizer
+
+//main f(x)
+int main(int argc, char** argv) {
+    //Declare variables
+    bool x, y;
+
+    //Row 1
+    x=true;
+    y=true;
+    cout << (x?'T':'F') << " ";
+    cout << (y?'T':'F') << "  ";
+    cout << (!x?'T':'F') << "   ";
+    cout << (!y?'T':'F') << "    ";
+    cout << (x||y?'T':'F') << " ";
+    cout<< endl;
+
+    //Row 2
+    x=true;
+    y=false;
+    cout << (x?'T':'F') << " ";
+    cout << (y?'T':'F') << "  ";
+    cout << (!x?'T':'F')<< "   ";
+
+    return 0;
+}
