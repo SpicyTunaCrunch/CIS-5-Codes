b7afb528399212a5e79a1567abd9ec48d39c96ac
Luis <luigy199601@gmail.com>
2026-03-26 13:56:27 -0700

    Lab Prof. loop examples

---
+++ lab/loop_demo.cpp
@@ -0,0 +1,35 @@
+/*
+*Looop Demo done in Lab
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    bool quit = false; 
+
+/*  for ( int i =0; i <= 10 ; i++ ){
+        cout << "i = " << i << endl;  //print i from 1-9
+    }
+
+    for ( int i = 10; i > 0; i--) {
+        cout << "i = " << i << endl; // print 10 -1
+    }
+
+    for (   int i = 10, j = 0; i > 0 && j < 5; i--,j++){
+        cout << ' i = ' << i << "   j = " << j << endl; // for mutliple variables
+     }
+*/
+    int i = 0;
+     do {
+        cout << "hello" << endl;
+        i++;
+    }   while ( i < 10); 
+
+    int i = 0;
+    while ( i < 10 ) {
+        
+    }
+
+    return 0;
+}
