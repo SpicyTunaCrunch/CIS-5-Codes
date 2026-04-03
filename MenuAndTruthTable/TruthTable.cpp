8c5c1fd6bfec99e28dce6ed670931e6efe76ac70
Luis <luigy199601@gmail.com>
2026-04-02 21:43:20 -0700

    April 2, 2026

--- MenuAndTruthTable/TruthTable.cpp
+++ MenuAndTruthTable/TruthTable.cpp
@@ -1,7 +1,7 @@
 /*
 *File: Turth Table
 *Author: Luis Roldan
-*Created  March 35, 2026
+*Created  April 2, 2026
 */
 
 //Sys Libraires
@@ -12,30 +12,83 @@ using namespace std; //Entity Organizer
 int main(int argc, char** argv) {
     //Declare variables
     bool x, y;
+    cout << "______________________________________________________________________________________________\n";
+    cout << "|                                       TRUTH TABLE                                          |\n";
+    cout << "______________________________________________________________________________________________\n";
+    cout << "|X| |Y|  |!X| |!Y| |X&&Y|  |X||Y| |X^Y| |X^Y^Y| |X^Y^X| |!(X&&Y)|  |!X||!Y| |!(X||Y)| |!X&&!Y|\n";
+    cout << "|''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''|\n";
 
     //Row 1
     x=true;
     y=true;
-    cout << (x?'T':'F') << " ";
-    cout << (y?'T':'F') << "  ";
-    cout << (!x?'T':'F') << "   ";
-    cout << (!y?'T':'F') << "    ";
-    cout << (x||y?'T':'F') << " ";
+    cout << "|" << (x?'T':'F') << "| |";
+    cout << (y?'T':'F') << "|  |";
+    cout << (!x?'T':'F') << " | | ";
+    cout << (!y?'T':'F') << "| |  ";
+    cout << (x&&y?'T':'F') << " |  |  ";
+    cout << (x||y?'T':'F') << " | | ";
+    cout << (x^y?'T':'F') << " | |  ";
+    cout << (x^y^y?'T':'F') << "  | |  ";
+    cout << (x^y^x?'T':'F') << "  | |   ";
+    cout << (!(x&&y)?'T':'F') << "   |  |   ";
+    cout << (!x||!y?'T':'F') << "  | |   ";
+    cout << (!(x||y)?'T':'F') << "   | |   ";
+    cout << (!x&&!y?'T':'F') << "  |";
     cout<< endl;
 
     //Row 2
     x=true;
     y=false;
-    cout << (x?'T':'F') << " ";
-    cout << (y?'T':'F') << "  ";
-    cout << (!x?'T':'F')<< "   ";
-    cout << (!y?'T':'F') << "    ";
-    cout << (x||y?'T':'F') << " ";
+    cout << "|" << (x?'T':'F') << "| |";
+    cout << (y?'T':'F') << "|  |";
+    cout << (!x?'T':'F') << " | | ";
+    cout << (!y?'T':'F') << "| |  ";
+    cout << (x&&y?'T':'F') << " |  |  ";
+    cout << (x||y?'T':'F') << " | | ";
+    cout << (x^y?'T':'F') << " | |  ";
+    cout << (x^y^y?'T':'F') << "  | |  ";
+    cout << (x^y^x?'T':'F') << "  | |   ";
+    cout << (!(x&&y)?'T':'F') << "   |  |   ";
+    cout << (!x||!y?'T':'F') << "  | |   ";
+    cout << (!(x||y)?'T':'F') << "   | |   ";
+    cout << (!x&&!y?'T':'F') << "  |";
+    cout<< endl;
 
     //Row 3
-
+    x=false;
+    y=true;
+    cout << "|" << (x?'T':'F') << "| |";
+    cout << (y?'T':'F') << "|  |";
+    cout << (!x?'T':'F') << " | | ";
+    cout << (!y?'T':'F') << "| |  ";
+    cout << (x&&y?'T':'F') << " |  |  ";
+    cout << (x||y?'T':'F') << " | | ";
+    cout << (x^y?'T':'F') << " | |  ";
+    cout << (x^y^y?'T':'F') << "  | |  ";
+    cout << (x^y^x?'T':'F') << "  | |   ";
+    cout << (!(x&&y)?'T':'F') << "   |  |   ";
+    cout << (!x||!y?'T':'F') << "  | |   ";
+    cout << (!(x||y)?'T':'F') << "   | |   ";
+    cout << (!x&&!y?'T':'F') << "  |";
+    cout<< endl;
     //Row 4
-
+    x=false;
+    y=false;
+    cout << "|" << (x?'T':'F') << "| |";
+    cout << (y?'T':'F') << "|  |";
+    cout << (!x?'T':'F') << " | | ";
+    cout << (!y?'T':'F') << "| |  ";
+    cout << (x&&y?'T':'F') << " |  |  ";
+    cout << (x||y?'T':'F') << " | | ";
+    cout << (x^y?'T':'F') << " | |  ";
+    cout << (x^y^y?'T':'F') << "  | |  ";
+    cout << (x^y^x?'T':'F') << "  | |   ";
+    cout << (!(x&&y)?'T':'F') << "   |  |   ";
+    cout << (!x||!y?'T':'F') << "  | |   ";
+    cout << (!(x||y)?'T':'F') << "   | |   ";
+    cout << (!x&&!y?'T':'F') << "  |";
+    cout<< endl;
+    cout << "''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''";
     //Exit stage left or right
     return 0;
 }
