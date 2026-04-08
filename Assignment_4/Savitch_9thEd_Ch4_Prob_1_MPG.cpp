61c7201aa45a570d8394cc87871bbd6f3de9fdb3
Luis <luigy199601@gmail.com>
2026-04-07 21:25:57 -0700

    april 7

--- Assignment_4/Savitch_9thEd_Ch4_Prob_1_MPG.cpp
+++ Assignment_4/Savitch_9thEd_Ch4_Prob_1_MPG.cpp
@@ -10,6 +10,51 @@
 #include <iomanip> //formating library
 using namespace std; //entity organizer
 
+//Global Constant
+const float GAL = 0.264179f;
+
+//f(x)s
+float mpg (float mile, float liter){
+    return mile / (liter * GAL);    }
+
+//main f(x)
+int main(int argc, char** argv){
+    
+    //Delcare Valriables
+    float 
+    liter, mile, result;
+    char
+    again; //repeat loop? 
+    
+    //initilaizatoion
+    cout << fixed << setprecision(2);
+    do {
+        if ( again == 'y' ) {
+            cout << endl;   }
+    cout << "Enter number of liters of gasoline:\n\n";
+    cin >> liter;
+    cout << "Enter number of miles traveled:\n\n";
+    cin >> mile;
+    
+    //Calling f(x)
+    result =mpg(mile, liter);
+    cout << "miles per gallon:\n" << result << endl;
+    cout << "Again:\n";
+    cin >> again;
+
+    }   while (again =='y');
+
+
+return 0;
+}
+
+/* //How I originally wrote it before using function
+
+//sys libraries
+#include <iostream> // Inpput-Output Library
+#include <iomanip> //formating library
+using namespace std; //entity organizer
+
 //main f(x)
 int main(int argc, char** argv){
     //declare Variables
@@ -42,3 +87,4 @@ int main(int argc, char** argv){
 
 return 0;
 }
+*/
