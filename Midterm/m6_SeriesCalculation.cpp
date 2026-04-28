0a0db5e28408f69ea6674671e0b8a81d07b63649
Luis <luigy199601@gmail.com>
2026-04-27 21:52:19 -0700

    april 27

---
+++ Midterm/m6_SeriesCalculation.cpp
@@ -0,0 +1,31 @@
+/*
+*File: Midterm Question 6 Series Calculation
+*Author: Luis Roldan
+*Created on April 27, 2026
+*Purpose: Just another sequence, 
+*   f(x) = x - x^3/3! + x^5/5! - x^7/7! + ............
+*   Input x and the number of terms used in the sequence, output the result.
+*   Note:  x^1 = x,   x^2 = x*x,  x^3 = x*x*x, 3! = 1*2*3,  5!=1*2*3*4*5
+*/
+
+//sysLibraries
+#include <iostream> // input-output library
+#include <iomanip> // Formatting library
+using namespace std;
+
+//prototypes
+
+//main f(x)
+int main(int argc, char** argv){
+    //Variables
+    float x,fx,term;
+    int nterms;
+     
+    cout << "Calculate a series f(x)=x-x^3/3!+x^5/5!-x^7/7!+......." << endl;
+    cout << "Input x and the number of terms, output f(x)" << endl;
+    cin >> x >> nterms;
+    
+    return 0;
+}
+
+//define  f(x)
