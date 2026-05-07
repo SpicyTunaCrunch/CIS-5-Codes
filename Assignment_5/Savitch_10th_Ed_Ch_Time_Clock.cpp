0ba186a97803072bca7da75930d166351cf68017
Luis <luigy199601@gmail.com>
2026-05-07 09:52:29 -0700

    May 7

---
+++ Assignment_5/Savitch_10th_Ed_Ch_Time_Clock.cpp
@@ -0,0 +1,25 @@
+/*
+*File: Savitch 10th Edition Chapter 5 Problem 2/3 Time Clock
+*Author: Luis Roldan
+*Created on May 5, 2026
+*Purpose: 2. Write a program that requests the current time and a waiting time as two integers 
+*            for the number of hours and the number of minutes to wait. The program then outputs 
+*            what the time will be after the waiting period. Use 24-hour notation for the times. 
+*            Include a loop that lets the user repeat this calculation for additional input values 
+*            until the user says she or he wants to end the program.
+*         3. Modify your program for Programming Project 2 so that it uses 12-hour notation, such as 3:45 PM
+*            Sample inputs will be on 1 line using the following format
+*            (hrs) (mins) (ampm) (wait time) (run again)     (hrs) (mins) (ampm) (wait time) (run again) ..................
+*/
+
+//syslibrary
+#include <iostream> //input-output library
+using namespace std; //entity organizer
+
+//prototypes
+
+//mainf(x)
+int maim(int argc, char** argv){
+
+}
+//defining f(x)
