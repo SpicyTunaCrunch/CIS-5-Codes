1392029ae5b33f4bd854a20e9345f3698720ba2c
Luis <luigy199601@gmail.com>
2026-05-07 23:57:48 -0700

    May 7

--- Assignment_5/Q5_CollatzSequence_OutputSequence.cpp
+++ Assignment_5/Q5_CollatzSequence_OutputSequence.cpp
@@ -18,13 +18,14 @@ int collatz(int);
 //main f(x)
 int main(int argc, char** argv){
     //Variables
-    int n;
+    int n, ns;
     //initializaton
     cout << "Collatz Conjecture Test\nInput a sequence start\n";
     cin >> n;
-
+    cout << n << ", ";
+    ns = collatz(n);
     //mapping
-    cout << "Sequence start of " << n << " cycles to 1 in "<< collatz(n) << " steps";
+    cout << "Sequence start of " << n << " cycles to 1 in "<< ns << " steps";
     return 0;
 }
 int collatz(int n){
@@ -33,10 +34,15 @@ int collatz(int n){
         if (n % 2 == 0){
         n /= 2;
     }
-    else {
+        else {
         n *= 3;
         n +=1;
     }
+    cout << n;
+    if (n != 1){
+        cout <<", ";    }
+    else {
+        cout << endl;   }
     i++;
     }
     return i;
