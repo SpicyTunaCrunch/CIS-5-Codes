72a889f96c39b8a41a3771da0a3f00b5e28dfae0
Luis <luigy199601@gmail.com>
2026-05-13 20:31:55 -0700

    May 13

--- Assignment_5/Savitch_9thEd_Ch5_Prob1_TImeConverter.cpp
+++ Assignment_5/Savitch_9thEd_Ch5_Prob1_TImeConverter.cpp
@@ -10,25 +10,52 @@
 using namespace std; //entity organizer
 
 //prototypes
-
+void input(int &hr, int &min);
+void convrt(int &hr,char &mer);
+void output(int hr, int min, char mer);
 //main f(x)
 int main(int argc, char** argv){
-
+    int 
+    hr, //hour
+    min; //minutes
+    char
+    mer, //meridiem
+    again; //repeat program?
+    do {
+    input(hr, min);
+    convrt (hr, mer);
+    output(hr, min, mer);
+    cout << "Would you like to convert another time (y/n)\n";
+    cin >> again;
+    } while (again == 'y' || again == 'Y');
+    return 0;
 }
 //defining f(x)
-
-
-/*      output expected:
-Military·Time·Converter·to·Standard·Time↵
-Input·Military·Time·hh:mm↵
-14:25·=·2:25·PM↵
-Would·you·like·to·convert·another·time·(y/n)↵
-00:15·=·12:15·AM↵
-Would·you·like·to·convert·another·time·(y/n)↵
-12:15·=·12:15·PM↵
-Would·you·like·to·convert·another·time·(y/n)↵
-12:69·is·not·a·valid·time↵
-Would·you·like·to·convert·another·time·(y/n)↵
-25:25·is·not·a·valid·time↵
-Would·you·like·to·convert·another·time·(y/n)↵
-*/
+void input(int &hr, int &min){
+    cout << "Military Time Converter to Standard Time\n";
+    cout << "Input Military Time hh:mm\n";
+    cin >> hr >> min;
+    cout << hr << ":" << min << " = ";
+}
+void convrt(int &hr, char &mer){
+    if (hr == 0){
+        hr = 12;
+        mer = 'A';  }
+    else if (hr < 12){
+        mer = 'A';  }
+    else if ( hr == 12){
+        mer = 'P';  }
+    else {
+        hr -= 12;
+        mer = 'P';  }
+}
+void output(int hr, int min, char mer){
+    cout << hr << ":";
+    if (min < 20){
+        cout << "0";    }
+    cout << min << " ";
+    if (mer == 'A'){
+        cout << "AM\n";     }
+    else {
+        cout << "PM\n";     }
+}
